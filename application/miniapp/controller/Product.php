6cd272bf45f71ea13852f24778f9500ec7f0f1c9
buffalo <451525896@qq.com>
2019-04-28 22:20:56 +0800

    商品新增类别及是否属于境外商品

--- application/miniapp/controller/Product.php
+++ application/miniapp/controller/Product.php
@@ -13,6 +13,7 @@ namespace app\miniapp\controller;
 use app\common\model\Product as ProductModel;
 use think\Db;
 use think\facade\Request;
+use think\File;
 use think\facade\Url;
 
 class Product extends Base
@@ -33,21 +34,40 @@ class Product extends Base
     {
         $where = [];
         $name = '';
+        $cate_id = '';
         if (Request::isGet()) {
             $_data = input('get.');
             $name = isset($_data['name']) ? $_data['name'] : '';
+            $cate_id = isset($_data['cate_id']) ? $_data['cate_id'] : '';
             $where = [];
-            if (isset($_data['name'])) {
+            if (!empty($_data['name'])) {
                 $where[] = ['name', 'like', "%{$_data['name']}%"];
+            }
+            if (!empty($_data['cate_id'])) {
+                $where[] = ['cate_id', '=', $_data['cate_id']];
             }
 
         }
         $post['name'] = $name;
+        $post['cate_id'] = $cate_id;
         $model = new ProductModel();
         $data = $model->where('status', '<>', 1)
             ->where($where)
             ->paginate(15);
         $page = $data->render();
+        // 获取商品分类
+        $cateData = $this->getCateList();
+        // 处理分类
+        foreach ($data as $key => $value) {
+            $data[$key]['cate'] = '';
+            foreach ($cateData as $row) {
+                if ($row['id'] == $value['cate_id']) {
+                    $data[$key]['cate'] = $row['name'];
+                    break;
+                }
+            }
+        }
+        $this->assign('cateData', $cateData);
         $this->assign('data', $data);
         $this->assign('post', $post);
         $this->assign('page', $page);
@@ -57,20 +77,30 @@ class Product extends Base
 
     public function add()
     {
+        $cateData = $this->getCateList();
+        $this->assign('cateData', $cateData);
         $this->assign('uploadImg', getHostDomain() . \url('uploadMediaNewsImage'));
         return view('edit');
     }
 
     public function edit()
     {
+        $cateData = $this->getCateList();
+        $this->assign('cateData', $cateData);
         $this->assign('uploadImg', getHostDomain() . \url('uploadMediaNewsImage'));
         $id = input('id');
-        $data = Db::name('product')->where('id',$id)->find();
+        $data = Db::name('product')->where('id', $id)->find();
         // 多图处理
         $data['pics'] = explode(',', $data['pics']);
 
-        $this->assign('data',$data);
+        $this->assign('data', $data);
         return view('edit');
+    }
+
+    public function getCateList()
+    {
+        $cateData = Db::name('cate')->where('is_del', 0)->select();
+        return $cateData;
     }
 
     public function save()
@@ -93,7 +123,7 @@ class Product extends Base
             }
 
             if ($id) {
-                if (Db::name('product')->where(['id'=>$id])->update($_data)) {
+                if (Db::name('product')->where(['id' => $id])->update($_data)) {
                     ajaxMsg(1, '修改成功');
                 } else {
                     ajaxMsg(0, '修改失败，没有做任何修改！');
@@ -114,7 +144,7 @@ class Product extends Base
         if (Request::isPost()) {
             $id = input('post.id');
             if ($id) {
-                if (Db::name('product')->where(['id'=>$id])->update(array('status'=>1))) {
+                if (Db::name('product')->where(['id' => $id])->update(array('status' => 1))) {
                     ajaxMsg(1, '删除成功');
                 } else {
                     ajaxMsg(0, '删除失败');
@@ -130,7 +160,42 @@ class Product extends Base
         if (empty($id) || empty($q_id)) {
             $this->error('参数错误！');
         }
-        Db::name('guess_question')->where('id',$q_id)->update(array('right_option'=>$id));
-        $this->redirect('index',array('q_id'=>$q_id));
+        Db::name('guess_question')->where('id', $q_id)->update(array('right_option' => $id));
+        $this->redirect('index', array('q_id' => $q_id));
     }
+
+    public function import()
+    {
+        $file = Request::file('file');
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        if($file){
+            $path = ROOT_PATH . 'public' . '/' . 'uploads';
+            $info = $file->move($path);
+            if($info){
+                // 获取文件后缀
+                $ext = $info->getExtension();
+                if ($ext != 'xls' && $ext != 'xlsx') {
+                    ajaxMsg('1', '上传格式不对！非（xls | xlsx）');
+                }
+                // excel上传成功后，执行Excel导入
+                $filename = $path . '/' . date('Ymd') . '/' . $info->getFilename();
+                /*vendor("phpexcel.PHPExcel");
+                $objReader =\PHPExcel_IOFactory::createReader('Excel2007');
+                $PHPExcel = $objReader->load($filename, $encode = 'utf-8');*/
+                //var_dump($PHPExcel);die;
+                // 成功上传后 获取上传信息
+                // 输出 jpg
+                //echo $info->getExtension();
+                // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+                //echo $info->getSaveName();
+                // 输出 42a79759f284b767dfcb2a0197904287.jpg
+                ajaxMsg('0', $url);
+                echo $info->getFilename();die;
+            }else{
+                // 上传失败获取错误信息
+                ajaxMsg('1', $file->getError());
+            }
+        }
+    }
+
 }
