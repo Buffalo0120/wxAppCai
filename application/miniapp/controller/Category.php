2988fab7acfe546215c688439d9d2dbdfb0b1f41
buffalo <451525896@qq.com>
2019-05-22 21:23:22 +0800

    分类管理编辑功能完善

--- application/miniapp/controller/Category.php
+++ application/miniapp/controller/Category.php
@@ -54,11 +54,39 @@ class Category extends Base
 
     public function save()
     {
+        if (Request::isPost()) {
+            $_data = input('post.');
+            $id = $_data['id'];
+            unset($_data['id']);
 
+            if ($id) {
+                if (Db::name('cate')->where(['id' => $id])->update($_data)) {
+                    ajaxMsg(1, '修改成功');
+                } else {
+                    ajaxMsg(0, '修改失败，没有做任何修改！');
+                }
+            } else {
+                $_data['add_time'] = time();
+                if (Db::name('cate')->insert($_data)) {
+                    ajaxMsg(1, '增加成功');
+                } else {
+                    ajaxMsg(0, '增加失败');
+                }
+            }
+        }
     }
 
     public function del()
     {
-
+        if (Request::isPost()) {
+            $id = input('post.id');
+            if ($id) {
+                if (Db::name('cate')->where(['id' => $id])->update(array('is_del' => 1))) {
+                    ajaxMsg(1, '删除成功');
+                } else {
+                    ajaxMsg(0, '删除失败');
+                }
+            }
+        }
     }
 }
